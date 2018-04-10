9c712b558e0dde26c450851178489d65b49ba667
madycakes <viclee02@gmail.com>
2018-04-10 13:53:41 -1000

    adds news exercises

---
+++ exercise.js
@@ -0,0 +1,81 @@
+/*Congratulations Prepsters, you've been selected for a top secret mission. Should you choose to accept it, do a git fetch and a git pull to find your specific mission. After you complete your mission, commit your work and do a pull request.
+
+This repo will self-destruct in five seconds. Good luck!*/
+
+/*Mission 1: 
+Team Member: 
+
+@param Datatype: String
+@return Datatype: String
+
+Create a function named weatherReport that will take in parameter weather and returns the following:
+"Today's forecast is weather." Where your input will replace the weather parameter.
+Console.log to see your result.*/
+
+/*Mission 2: 
+Team Member: 
+
+@param Datatype: String
+@param Datatype: Number
+@return Datatype: String
+
+Create a function named happyBday that will take in two parameters name and age and returns the following:
+"Happy Birthday name! You look awesome at age!" Where your input will replace the name and age parameter.
+Console log to see your result.*/
+
+/*Mission 3: 
+Team Member: 
+
+@param Datatype: Array
+@return Datatype: Array
+
+Create a function named doubleUp that will take in a parameter arr and will double the value of each number in the array and will return the array.
+Console.log your result.*/
+
+var numArr = [1, 6, 8];
+
+
+/*Mission 4: 
+Team Member: 
+
+@param Datatype: Array
+@return Datatype: Array
+
+Create a function named evens that will take in a parameter arr and will sum up only the even indexed numbers in the array and will return a new array.
+Console.log your result.*/
+
+var evenArr = [1,2,3,4,5,6,7,8];
+
+/*Mission 5:
+Team Member: 
+
+@param Datatype: String
+@param Datatype: Array
+
+Create a function named plusOne that will take in a parameter str and will add a new element into an array
+Console.log your result*/
+
+var favFoods = ['curry ramen', 'carrot cake', 'oreos', 'bacon cheese burger', 'char siu bau'];
+
+/*Mission 6:
+Team Member: 
+
+@param Datatype: Number
+@return Datatype: Number
+
+Create a function named converter that will convert dollars to yen based on today's foreign exchange rate.
+Console.log your result*/
+
+/*Mission 7:
+Team Member: Cyrus
+
+Create a function that will convert miles to kilometers.*/
+
+function milesToKilometers(miles) {
+	return miles * 1.60934;
+}
+
+
+
+
+
