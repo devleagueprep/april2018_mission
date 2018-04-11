3159226ee11a2ba79ee9cd5d0633eb9fc9fe7b98
madycakes <viclee02@gmail.com>
2018-04-10 15:00:16 -1000

    complete exercises

--- exercise.js
+++ exercise.js
@@ -8,9 +8,9 @@ Team Member:
 @param Datatype: String
 @return Datatype: String
 
-Create a function named weatherReport that will take in parameter weather and returns the following:
-"Today's forecast is weather." Where your input will replace the weather parameter.
+Create a function named weatherReport that takes a parameter weather and returns the following: "Today's forecast is weather." Where your input will replace the weather parameter.
 Console.log to see your result.*/
+
 
 /*Mission 2: 
 Team Member: 
@@ -19,9 +19,9 @@ Team Member:
 @param Datatype: Number
 @return Datatype: String
 
-Create a function named happyBday that will take in two parameters name and age and returns the following:
-"Happy Birthday name! You look awesome at age!" Where your input will replace the name and age parameter.
+Create a function named happyBday that takes two parameters name and age and returns the following: "Happy Birthday name! You look awesome at age!" Where your input will replace the name and age parameters.
 Console log to see your result.*/
+
 
 /*Mission 3: 
 Team Member: 
@@ -29,7 +29,7 @@ Team Member:
 @param Datatype: Array
 @return Datatype: Array
 
-Create a function named doubleUp that will take in a parameter arr and will double the value of each number in the array and will return the array.
+Create a function named doubleUp that takes in a parameter arr and will double the value of each number in the array and returns the array.
 Console.log your result.*/
 
 var numArr = [1, 6, 8];
@@ -41,21 +41,24 @@ Team Member:
 @param Datatype: Array
 @return Datatype: Array
 
-Create a function named evens that will take in a parameter arr and will sum up only the even indexed numbers in the array and will return a new array.
+Create a function named evens that takes in a parameter arr and will sum up only the even indexed numbers in the array and returns a new array.
 Console.log your result.*/
 
 var evenArr = [1,2,3,4,5,6,7,8];
 
+
 /*Mission 5:
 Team Member: 
 
+@param Datatype: Array
 @param Datatype: String
 @param Datatype: Array
 
-Create a function named plusOne that will take in a parameter str and will add a new element into an array
+Create a function named plusOne that takes two parameters arr and str and will add a new string element into an array and will return the array.
 Console.log your result*/
 
 var favFoods = ['curry ramen', 'carrot cake', 'oreos', 'bacon cheese burger', 'char siu bau'];
+
 
 /*Mission 6:
 Team Member: 
@@ -63,17 +66,58 @@ Team Member:
 @param Datatype: Number
 @return Datatype: Number
 
-Create a function named converter that will convert dollars to yen based on today's foreign exchange rate.
+Create a function named yenConverter that takes a parameter rate and will convert dollars to yen based on today's foreign exchange rate and returns the new yen amount.
 Console.log your result*/
 
+
 /*Mission 7:
-Team Member: Cyrus
+Team Member: 
 
-Create a function that will convert miles to kilometers.*/
+@param Datatype: Number
+@return Datatype: Number
 
-function milesToKilometers(miles) {
-	return miles * 1.60934;
-}
+Create a function named kmConverter that takes in a parameter miles and will convert miles to kilometers and returns the new kilometer amount
+Console.log your result.*/
+
+
+/*Mission 8:
+Team Member: 
+
+@param Datatype: Number
+@return Datatype: String
+
+Create a function named whatToEat that takes in a parameter budget. Create the following return statements based on the budget amount: 
+
+<=5 => "Mickey D's"
+<=10 => "Panda Express"
+>10 => "Peppas"
+
+Console.log your result*/
+
+
+/*Mission 9:
+Team Member: 
+
+@param Datatype: Array
+@return Datatype: Array
+
+Create a function named countUp that takes in a parameter arr and will add 1 to each number in the array and returns the array.
+Console.log you result.*/
+
+var numBox = [6, 24, 16, 2, 3];
+
+
+/*Mission 10:
+Team Member: 
+
+@param Datatype: Array
+@return Datatype: Array
+
+Create a function named backwards that takes in a parameter arr and returns the array in reverse order. Use a reverse for loop. 
+Console.log you result.*/
+
+
+
 
 
 
