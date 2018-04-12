34892f9f55c02a4b247b6c41cb4623f4065d32b8
velzland <valzey.freitas@gmail.com>
2018-04-11 19:47:35 -1000

    attemped no 1

--- exercise.js
+++ exercise.js
@@ -11,6 +11,11 @@ Team Member: Valzey
 Create a function named weatherReport that takes a parameter weather and returns the following: "Today's forecast is weather." Where your input will replace the weather parameter.
 Console.log to see your result.*/
 
+function weatherReport(weather){
+   if weather
+    return "Today's forecast is weather"
+}
+console.log(weatherReport(rain)
 
 /*Mission 2: 
 Team Member: Camille
