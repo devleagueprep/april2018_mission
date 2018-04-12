1325cef86c1f49a5d6d17e9519bbff84839627ed
Sarah <yamashige.sarah@gmail.com>
2018-04-11 19:51:36 -1000

    Brain not functioning.

--- exercise.js
+++ exercise.js
@@ -34,13 +34,14 @@ Console.log your result.*/
 
 var numArr = [1, 6, 8];
 
+/*
 function doubleUp (arr) {
     for (var i = 0; i < arr.length; i++) {
-        arr[i]*2; 
     }
-    return arr;
+    return arr[i]*2;
 }
 console.log(doubleUp(numArr))
+*/
 
 /*Mission 4: 
 Team Member: Erin
